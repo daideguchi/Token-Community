bd694616450e566d53fd7290d35c2c48da006494
daideguchi <dd.1107.11107@gmail.com>
2022-08-20 23:29:41 +0900

    MemberNFT:NFT作成テストの実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -1,4 +1,4 @@
-const { expect } = require("Chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("MemberNFTコントラクト", function () {
@@ -6,14 +6,17 @@ describe("MemberNFTコントラクト", function () {
   let memberNFT;
   const name = "MemberNFT";
   const symbol = "MEM";
+  const tokenURI1 = "hoge1";
+  const tokenURI2 = "hoge2";
   let owner;
+  let addr1;
 
   //何度も同じ処理をしなくて良いように共通化をする。。。beforeEachを使う
   beforeEach(async function () {
     //getSigner"s"は持っているアドレス全部を持ってくるという意味
-    //持ってきたアドレスの先頭のものをownerに入れる
+    //持ってきたアドレスの先頭のものをownerに入れる,二番目はaddr1
     // const [owner] = await ethers.getSigners();
-    [owner] = await ethers.getSigners();
+    [owner, addr1] = await ethers.getSigners();
     /** ここからデプロイの準備ーーーーーーーーーーーー*/
     MemberNFT = await ethers.getContractFactory("MemberNFT");
     memberNFT = await MemberNFT.deploy(); //ここでデプロイできる
@@ -32,5 +35,21 @@ describe("MemberNFTコントラクト", function () {
     /** ここからテストの準備ーーーーーーーーーーーー*/
     expect(await memberNFT.owner()).to.equal(owner.address); //.addressを記述する必要あり
     /**ここまでーーーーーーーーーーーー */
+  });
+  it("onwerはNFTを作成できるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    expect(await memberNFT.ownerOf(1)).to.equal(addr1.address);
+  });
+  it("NFT作成のたびにtokenIdがインクリメントされるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    await memberNFT.nftMint(addr1.address, tokenURI2);
+    expect(await memberNFT.tokenURI(1)).to.equal(tokenURI1);
+    expect(await memberNFT.tokenURI(2)).to.equal(tokenURI2);
+  });
+    it("owner以外はNFT作成に失敗すべき", async function () {
+      //connectでアドレスを指定
+      await expect(
+        memberNFT.connect(addr1).nftMint(addr1.address, tokenURI1)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
   });
 });

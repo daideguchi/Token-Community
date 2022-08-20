02f7be03bb6e42d0aad5c38b9a2c5db7df8d69e5
daideguchi <dd.1107.11107@gmail.com>
2022-08-20 16:26:11 +0900

    MenberNFT:OZインポートと継承、constructor定義

--- contracts/MenberNFT.sol
+++ contracts/MenberNFT.sol
@@ -1,6 +1,42 @@
 // SPDX-License-Identifier: MIY
 pragma solidity ^0.8.9;
 
-contract MenberNFT{
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; //オーナーのみ表示させる
+import "@openzeppelin/contracts/utils/Counters.sol"; //カウントをインクリメント
+
+contract MenberNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
+    constructor() ERC721("MenberNFT", "MEM") {}
+
+    /**
+     * @dev
+     * オーバーライド
+     */
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
 
 }

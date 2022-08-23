717f4cbacc9c7c85f611ae30ba1fb059fca4cc29
daideguchi <dd.1107.11107@gmail.com>
2022-08-23 20:07:31 +0900

    MemberNFT:開発

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 //.envをインポート
 require("dotenv").config();
+//hardhatにverifyを追加  verify: Verifies contract on Etherscan
+require("@nomiclabs/hardhat-etherscan");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -13,6 +15,9 @@ module.exports = {
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
-    },
+    }
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };

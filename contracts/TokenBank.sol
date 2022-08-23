0c8e6540b91738d338ca14d57256e443a9cda72b
daideguchi <dd.1107.11107@gmail.com>
2022-08-23 21:46:44 +0900

    TokenBank:イベント定義

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -4,24 +4,44 @@ pragma solidity ^0.8.9;
 
 //状態変数の定義
 contract TokenBank {
-    // Tokenの名前
+    /// @dev Tokenの名前
     string private _name;
 
-    // Tokenのシンボル
+    /// @dev Tokenのシンボル
     string private _symbol;
 
-    // Tokenの総供給量数 定数なのでconstantにする
+    /// @dev Tokenの総供給量数 定数なのでconstantにする
     uint256 constant _totalSupply = 1000;
 
-    // TokenBnakが預かっているTokenの総額
+    /// @dev TokenBnakが預かっているTokenの総額
     uint256 private _bankTotalDeposit;
 
-    // TokenBankのオーナーのアドレスを格納
+    /// @dev TokenBankのオーナーのアドレスを格納
     address public owner;
 
-    // アカウントアドレスごとのToken残高 addressを入力するとuint256型が返ってくる
+    /// @dev アカウントアドレスごとのToken残高 addressを入力するとuint256型が返ってくる
     mapping(address => uint256) private _balances;
 
-    // TokenBankが預かっているToken残高
+    /// @dev TokenBankが預かっているToken残高
     mapping(address => uint256) private _tokenBankBalances;
+
+    /// @dev Token移転時のイベント
+    event TokenTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @dev Token預入時のイベント
+    event TokenDeposit(
+        address indexed from,
+        uint amount
+    );
+
+    /// @dev Token引出時のイベント
+    event TokenWithdraw(
+        address indexed from,
+        uint amount
+    );
+
 }

1a4b0f75b81a05d288452849fff56d25b0c1a4c0
daideguchi <dd.1107.11107@gmail.com>
2022-08-31 20:38:22 +0900

    frontend:TokenBankへのクエリ結果の表示機能

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -43,6 +43,7 @@ export default function Home() {
 
       if (chain != rinkebyId) {
         alert("Rinkebyに接続してください!");
+        setChainId(false);
         return;
       } else {
         setChainId(true);
@@ -170,8 +171,29 @@ export default function Home() {
           >
             MetaMaskを接続
           </button>
+        ) : chainId ? (
+          <div>
+            <div className="px-2 py-2 bg-transparent">
+              <span className="flex flex-col items-left font-semibold">
+                総預かり残高：{bankTotalDeposit}
+              </span>
+            </div>
+            <div className="px-2 py-2 mb-2 bg-white border border-gray-400">
+              <span className="flex flex-col items-left font-semibold">
+                アドレス：{account}
+              </span>
+              <span className="flex flex-col items-left font-semibold">
+                所持残高：{tokenBalance}
+              </span>
+              <span className="flex flex-col items-left font-semibold">
+                預入残高：{bankBalance}
+              </span>
+            </div>
+          </div>
         ) : (
-          <></>
+          <div className="flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3">
+            <div>Rinkebyに接続してください</div>
+          </div>
         )}
       </div>
     </div>
